abea3de4a8c889a185cc2ab64137ffec0e9483ef
skykuba <s198088@student.pg.edu.pl>
2025-11-19 17:18:39 +0100

    Add address repository

---
+++ src/main/java/org/example/aui/repository/AddressRepository.java
@@ -0,0 +1,8 @@
+package org.example.aui.repository;
+import org.example.aui.entity.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+@Repository
+public interface AddressRepository extends JpaRepository<Address, UUID> {
+}
