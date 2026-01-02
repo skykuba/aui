c1083c93853d6258054c5616f298aaccd6d4217d
skykuba <s198088@student.pg.edu.pl>
2026-01-02 19:18:43 +0100

    Refactor - remove unused imports in create-client.ts

--- frontend/src/app/components/create-client/create-client.ts
+++ frontend/src/app/components/create-client/create-client.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import {ClientService} from '../../services/client/client';
-import {Router} from '@angular/router';
+import { Component } from '@angular/core';
 import {ClientForm} from '../client-form/client-form';
 
 @Component({
