d1d248b8c30c796683e54ad807c682938592198c
skykuba <s198088@student.pg.edu.pl>
2026-01-14 18:14:41 +0100

    Enhance update invoice component: implement invoice retrieval and conditional rendering

--- frontend/src/app/components/update-invoice/update-invoice.ts
+++ frontend/src/app/components/update-invoice/update-invoice.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { InvoiceForm } from '../invoice-form/invoice-form';
+import { ActivatedRoute } from '@angular/router';
+import { InvoiceService } from '../../services/invoice/invoice';
+import { CreateOrUpdateInvoiceInterface } from '../../models/invoice.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-update-invoice',
-  imports: [InvoiceForm],
+  imports: [InvoiceForm, CommonModule],
   templateUrl: './update-invoice.html',
   styleUrl: './update-invoice.css',
   standalone: true,
 })
-export class UpdateInvoice {
-  constructor() {}
-}
+export class UpdateInvoice implements OnInit {
+  invoice: CreateOrUpdateInvoiceInterface | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private invoiceService: InvoiceService
+  ) {}
+
+  ngOnInit(): void {
+    const invoiceId = this.route.snapshot.paramMap.get('invoiceId');
+    const clientId = this.route.snapshot.paramMap.get('clientId');
+    if (invoiceId && clientId) {
+      this.invoiceService
+        .getClientsInvoice(clientId, invoiceId)
+        .subscribe((invoice) => {
+          this.invoice = invoice;
+        });
+    }
+  }
+}
